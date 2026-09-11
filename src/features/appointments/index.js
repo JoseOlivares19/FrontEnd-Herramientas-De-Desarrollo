6ec5b301124a2018944786034a4ad40f61ffb52a
FabrizioMn <fabriziomn3@gmail.com>
2026-09-11 17:36:19 -0500

    agregar pagina y barrel de la feature appointments

---
+++ src/features/appointments/index.js
@@ -0,0 +1,2 @@
+export { default as AppointmentBookingPage } from './pages/AppointmentBookingPage'
+export { default as AppointmentStepper } from './components/AppointmentStepper'
---
+++ src/features/appointments/pages/AppointmentBookingPage.jsx
@@ -0,0 +1,9 @@
+import AppointmentStepper from '../components/AppointmentStepper'
+
+export default function AppointmentBookingPage() {
+  return (
+    <div className="appointment-page">
+      <AppointmentStepper />
+    </div>
+  )
+}
